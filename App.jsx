25c4a9eb59d6ac5c6eb87a940503ae84da3cf48d
ZacGeo <129080359+ZacGeo@users.noreply.github.com>
2024-07-21 15:54:14 +0300

    preparing for reset of the client side

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Welcome } from './pages/welcome';
+import { Information } from './pages/information';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/information" element={<Information />} />
+        <Route path="/" element={<Welcome />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
