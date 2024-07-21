57da433788c72e7e78a6b6ef202cafc68b9a5f8b
ZacGeo <129080359+ZacGeo@users.noreply.github.com>
2024-07-21 16:22:18 +0300

    reset again

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Welcome } from './pages/welcome';
 import { Information } from './pages/information';
 
+
+
 function App() {
   return (
     <Router>
