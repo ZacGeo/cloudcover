1461898a00edaf5a0b7627a434d3d6baf698a196
ZacGeo <129080359+ZacGeo@users.noreply.github.com>
2024-08-02 17:16:16 +0300

    some fixes

--- client/src/components/instructions/Good.jsx
+++ client/src/components/instructions/Good.jsx
@@ -1,13 +1,18 @@
+import { getCurrentTheme } from "../usefulFunctions/TimeTheme";
+
+const theme = getCurrentTheme();
+
+
 export function GoodNight() {
+
     return (
       <div
         style={{
-            background: "radial-gradient(at 88% 40%, hsla(120, 40%, 50%, 1) 0px, transparent 85%), radial-gradient(at 49% 30%, hsla(120, 40%, 60%, 1) 0px, transparent 85%), radial-gradient(at 14% 26%, hsla(120, 40%, 70%, 1) 0px, transparent 85%)"
-        }}
+            background: theme.secondary, color: theme.color}}
         className="flex rounded-xl shadow-md overflow-hidden md:max-w-2xl h-full mt-12 justify-center items-center"
       >
         <div className="p-8">
-          <p className="text-lg text-gray-300 font-sans font-bold">
+          <p className="text-lg font-sans font-bold">
             The weather is good.
           </p>
         </div>
@@ -19,12 +24,12 @@ export function Good() {
     return (
       <div
         style={{
-            background: "radial-gradient(at 88% 40%, hsla(120, 40%, 50%, 1) 0px, transparent 85%), radial-gradient(at 49% 30%, hsla(120, 40%, 60%, 1) 0px, transparent 85%), radial-gradient(at 14% 26%, hsla(120, 40%, 70%, 1) 0px, transparent 85%)"
+           background: theme.secondary, color: theme.color
         }}
         className="flex rounded-xl shadow-md overflow-hidden md:max-w-2xl h-full mt-12 justify-center items-center"
       >
         <div className="p-8">
-          <p className="text-lg text-gray-300 font-sans font-bold">
+          <p className="text-lg  font-sans font-bold">
             The weather is good.
           </p>
         </div>
